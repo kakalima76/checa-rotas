e5ef6b7f68eb0ae0546c0abc18d5c47d4aaaf8c2
kakalima76 <nieraldo@gmail.com>
2024-08-16 23:25:32 -0300

    termino da primeira parte

--- script.js
+++ script.js
@@ -14,15 +14,46 @@ new Vue({
     roteiro: null,
     polilinhas: [],
     trechos: [],
-    veiculo: null,
+    placa: null,
     data: null,
     inicio: null,
     fim: null,
     bearer: null,
+    show: true,
+    percurso: [],
+    telemetria: [],
+    ranger: 50,
+    pontos: [],
+    ponto: null,
+    intervalo: 120000,
+    showIntervalo: false,
+    liberacao: null,
   },
   watch: {
     trechos: function (n, o) {
       this.renderizarPolilinhas();
+    },
+
+    percurso: function (n, o) {
+      this.renderizarPercurso();
+    },
+
+    telemetria: function (newValue, oldValue) {
+      const max = Math.round(newValue.length / this.ranger);
+
+      newValue.forEach((n, i) => {
+        if (i % this.ranger === 0) {
+          this.adicionarMarcadorPersonalizado(
+            n.latitude,
+            n.longitude,
+            n.data,
+            i / this.ranger,
+            i / this.ranger === max ? true : false
+          );
+
+          this.pontos.push({ latitude: n.latitude, longitude: n.longitude });
+        }
+      });
     },
   },
   mounted() {
@@ -37,6 +68,30 @@ new Vue({
 
       // Adiciona um popup ao marcador com o nome e vetor
       marker.bindPopup(`<b>${nome}</b><br>${vetor}`).openPopup();
+    },
+    adicionarMarcadorPersonalizado(latitude, longitude, hora, numero, max) {
+      // Cria um ícone numerado usando a função criarIcone
+      const icon = this.criarIcone(numero, max);
+
+      // Cria um marcador usando as coordenadas fornecidas e o ícone numerado
+      const marker = L.marker([latitude, longitude], { icon }).addTo(this.map);
+
+      // Adiciona um popup ao marcador com o nome e vetor
+      marker.bindPopup(`<b>${hora}</b>`).openPopup();
+    },
+    adicionarTempo(milisegundos) {
+      const dataAtual = new Date();
+      const novaData = new Date(dataAtual.getTime() + milisegundos);
+
+      const horas = String(novaData.getHours()).padStart(2, "0");
+      const minutos = String(novaData.getMinutes()).padStart(2, "0");
+      this.liberacao = `Próxima consulta liberada apenas às ${horas}:${minutos}`;
+      this.showIntervalo = true;
+      Swal.fire("Aviso", this.liberacao, "warning");
+      setTimeout(() => {
+        this.showIntervalo = false;
+        Swal.fire("Aviso", "Nova consulta liberada.", "warning");
+      }, this.intervalo);
     },
     buscarRoteiro() {
       this.limparMapa();
@@ -74,7 +129,6 @@ new Vue({
                 this.trechos = this.polilinhas.filter(
                   (x) => x.opcao === "coleta"
                 );
-                console.log(this.trechos);
               }
             })
             .catch((error) =>
@@ -86,30 +140,62 @@ new Vue({
         );
     },
     buscarTelemetria() {
+      this.limparMapa();
+      this.buscarRoteiro();
+
       const roteiro = this.prefixos.filter((x) => x.roteiro === this.roteiro);
       const { inicio, fim } = roteiro[0];
-      console.log(inicio, fim);
 
       this.inicio = `${this.data} ${inicio.toString().padStart(2, "0")}:00:00`;
       this.fim = `${this.data} ${fim.toString().padStart(2, "0")}:00:00`;
 
-      const url = `${this.bysat}/getPosicaoTelemetria?id_veiculo=${this.veiculo}&periodo_inicio=${this.inicio}&periodo_fim=${this.fim}`;
-      console.log(url);
+      const data = {
+        bearer: this.bearer,
+        placa: this.placa,
+        inicio: this.inicio,
+        fim: this.fim,
+      };
+
+      console.log(data);
+
+      const url = `${this.base}/telemetria`;
 
       const config = {
-        method: "get",
+        method: "post",
         url,
         headers: {
           Authorization: `Bearer ${this.bearer}`,
         },
+        data,
       };
+
+      this.show = false;
 
       axios(config)
         .then((response) => {
-          console.log(response.data);
+          this.telemetria = response.data.map((d) => ({
+            empresa: d.empresa,
+            data: d.data_transmissao,
+            frota: d.frota,
+            id: d.id_veiculo,
+            latitude: d.latitude,
+            longitude: d.longitude,
+            motorista: d.nome_condutor,
+            uid: this.converterParaMilisegundos(d.data_transmissao),
+          }));
+
+          this.telemetria
+            .sort((a, b) => a.uid - b.uid)
+            .forEach((el) => {
+              this.percurso.push([el.latitude, el.longitude]);
+            });
+
+          this.show = true;
         })
+        .then(() => this.adicionarTempo(this.intervalo))
         .catch((error) => {
-          Swal.fire("Sucesso", error.message, "success");
+          this.show = true;
+          Swal.fire("Error", error.message, "error");
         });
     },
     carregarGerencias() {
@@ -125,24 +211,6 @@ new Vue({
         });
     },
     carregarLocais() {
-      fetch(`${this.base}/locais`)
-        .then((response) => response.json())
-        .then((data) => {
-          const tipos = new Set();
-          const locais = new Set();
-          data.forEach((item) => {
-            locais.add({
-              nome: item.nome,
-              tipo: item.tipo,
-            });
-            tipos.add(item.tipo);
-          });
-
-          this.locais = Array.from(locais);
-          this.tipos = Array.from(tipos);
-        });
-    },
-    carregarLocais() {
       fetch(`${this.base}/roteiros`)
         .then((response) => response.json())
         .then((data) => {
@@ -153,6 +221,30 @@ new Vue({
 
           this.prefixos = Array.from(prefixos);
         });
+    },
+    centralizarMapa(latitude, longitude, zoom = 16) {
+      this.map.setView([latitude, longitude], zoom);
+    },
+    converterParaMilisegundos(dataHora) {
+      const [data, hora] = dataHora.split(" ");
+      const [dia, mes, ano] = data.split("/");
+      const dataFormatada = `${ano}-${mes}-${dia}T${hora}`;
+      return new Date(dataFormatada).getTime();
+    },
+    criarIcone(text, max) {
+      const html =
+        text == "0" || max === true
+          ? `<div style="background-color: #8B0000; color: #FFFFFF; border-radius: 50%; width: 21px; height: 21px; display: flex; align-items: center; justify-content: center; font-size: 14px;">${text}</div>`
+          : `<div style="background-color: #1E90FF; color: #FFFFFF; border-radius: 50%; width: 21px; height: 21px; display: flex; align-items: center; justify-content: center; font-size: 14px;">${text}</div>`;
+
+      return L.divIcon({
+        html,
+        className: "", // Remova a classe padrão
+        iconSize: [21, 21],
+        iconAnchor: [10, 21],
+        popupAnchor: [1, -21],
+        shadowSize: [41, 41],
+      });
     },
     decodificarOverwiewPolyline(encoded) {
       let index = 0,
@@ -202,7 +294,20 @@ new Vue({
         }
       ).addTo(this.map);
     },
+    irParaPonto() {
+      if (this.ponto >= 0 && this.ponto < this.pontos.length) {
+        console.log();
+        const { latitude, longitude } = this.pontos[this.ponto];
+        this.centralizarMapa(latitude, longitude);
+      } else {
+        Swal.fire("Aviso", "Busque um ponto válido", "warning");
+      }
+    },
     limparMapa() {
+      this.polilinhas = [];
+      this.trechos = [];
+      this.percurso = [];
+      this.pontos = [];
       this.map.eachLayer((layer) => {
         if (layer instanceof L.TileLayer) {
           return; // Mantém a camada base
@@ -222,6 +327,18 @@ new Vue({
         this.map.fitBounds(polilinha.getBounds());
       });
     },
+    renderizarPercurso() {
+      const polilinha = L.polyline(this.percurso, {
+        color: "blue", // Cor fixa para a polilinha
+        weight: 1, // Espessura da linha
+        opacity: 0.5, // Opacidade da linha
+      }).addTo(this.map);
+
+      // Centralizar o mapa na primeira polilinha adicionada
+      if (polilinha) {
+        this.map.fitBounds(polilinha.getBounds());
+      }
+    },
     updateRoteiros() {
       this.roteiro = ""; // Reset the selected roteiro
     },
@@ -238,17 +355,24 @@ new Vue({
         .map((item) => item);
     },
     filteredPrefixos() {
-      const arr = new Set();
+      const arr = [];
 
       const filter = this.prefixos.filter(
         (item) => item.gerencia === this.gerencia
       );
 
-      filter.forEach((x) => {
-        arr.add(x.prefixo);
+      filter.forEach((el) => {
+        const index = arr.findIndex((x) => x.placa === el.placa);
+        if (index === -1) {
+          if (el.roteiro === this.roteiro) {
+            el.prefixo = el.prefixo + " " + "(previsto)";
+          }
+
+          arr.push(el);
+        }
       });
 
-      return Array.from(arr);
+      return arr.filter((x) => x.gerencia === this.gerencia);
     },
   },
 });
